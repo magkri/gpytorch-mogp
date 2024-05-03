81500b5737b81082f581737d7b0423d396d92a3e
magkri <magnus.kristiansen@dnv.com>
2024-05-03 16:40:14 +0200

    refactor(interface): remove monkey-patching

--- src/gpytorch_mogp/__init__.py
+++ src/gpytorch_mogp/__init__.py
@@ -1,17 +1,4 @@
-import gpytorch
-
-from gpytorch_mogp.likelihoods.multioutput_gaussian_likelihood import FixedNoiseMultiOutputGaussianLikelihood
 from gpytorch_mogp.kernels.multioutput_kernel import MultiOutputKernel
-
-# Monkey-patch gpytorch
-# This allows users to do:
-# >>> import gpytorch
-# >>> import gpytorch_mogp
-# >>> gpytorch.kernels.MultiOutputKernel
-# <class 'gpytorch_mogp.multioutput_kernel.MultiOutputKernel'>
-# >>> gpytorch.likelihoods.FixedNoiseMultiOutputGaussianLikelihood
-# <class 'gpytorch_mogp.multioutput_gaussian_likelihood.FixedNoiseMultiOutputGaussianLikelihood'>
-gpytorch.kernels.MultiOutputKernel = MultiOutputKernel
-gpytorch.likelihoods.FixedNoiseMultiOutputGaussianLikelihood = FixedNoiseMultiOutputGaussianLikelihood
+from gpytorch_mogp.likelihoods.multioutput_gaussian_likelihood import FixedNoiseMultiOutputGaussianLikelihood
 
 __all__ = ["MultiOutputKernel", "FixedNoiseMultiOutputGaussianLikelihood"]
